6cb82f8c30ab98a0f7e519b24b26ff59417edd02
Shardulkulk297 <shardulkulk297@gmail.com>
2024-06-05 00:35:51 +0530

    Inserting and removing HTML Elements using DOM, VIDEO 71

---
+++ inserting.js
@@ -0,0 +1,10 @@
+document.querySelector(".box").innerHTML
+document.querySelector(".box").outerHTML
+document.querySelector(".box").hasAttribute("style")
+document.querySelector(".box").getAttribute("style")
+document.querySelector(".box").setAttribute("style", "display: inline")
+//removeAttribute
+//attributes
+document.querySelector(".box").dataset
+//data-createdby = "" data-conceptby = ""
+
