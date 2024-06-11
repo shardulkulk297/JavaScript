151f6538ee0540fe152cca5b842996272feb9985
Shardulkulk297 <shardulkulk297@gmail.com>
2024-06-11 23:52:35 +0530

    OOP In JS

---
+++ Classobj.js
@@ -0,0 +1,42 @@
+class Animal{
+    constructor(name)
+    {
+        this.name = name;
+        console.log("Object is created")
+
+    }
+
+    eats()
+    {
+        console.log("eating")
+    }
+}
+
+let a = new Animal("bunny")
+console.log(a)
+a.eats()
+a.name
+
+class Lion extends Animal{ //inheritance
+    constructor(name){
+        super(name);
+        console.log("Object for lion is created")
+
+    }
+
+
+    roars()
+    {
+        console.log("Roaring")
+    }
+
+    eats() //method overriding
+    {
+        console.log("lion is eating")
+    }
+
+}
+
+let l = new Lion("shera")
+console.log(l)
+l.eats();
